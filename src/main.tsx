0bfcc1da712763815b72a7277098b335c7258dfe
afei <afei@66rpg.com>
2023-02-08 09:57:50 +0800

    style: svg icon align

--- src/main.tsx
+++ src/main.tsx
@@ -1,9 +1,9 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App'
+import { ThemeProvider } from './context/ThemeContext'
 import './index.css'
 import 'antd/dist/reset.css'
-import { ThemeProvider } from './context/ThemeContext'
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
